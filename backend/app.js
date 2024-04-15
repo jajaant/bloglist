a5186a42e5f21c701ec917723a913639823d6d2b
Jake Anttonen <jajaant@utu.fi>
2024-04-15 18:09:26 +0300

    4.5 valmistelut tehty

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const config = require('./utils/config')
-const http = require('http')
+require('express-async-errors')
 const express = require('express')
 const app = express()
 const cors = require('cors')
