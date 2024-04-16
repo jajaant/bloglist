1865e3b26d017a046a301cdc840bf49389d551a0
Jake Anttonen <jajaant@utu.fi>
2024-04-16 15:34:17 +0300

    4.10 api/users toimii

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
@@ -7,9 +8,23 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+
+  const user = await User.findById(body.userID)
+
+  console.log(user)
+
+  const blog = new Blog({
+    url: body.url,
+    title: body.title,
+    author: body.author,
+    likes: body.likes,
+    user: user._id,
+  })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
