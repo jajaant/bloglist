9218332d4a803a13f7c6a958e3112ac88a81dc7b
Jake Anttonen <jajaant@utu.fi>
2024-05-08 23:07:19 +0300

    5-9 kesken

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -52,7 +52,8 @@ blogsRouter.put('/:id', async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: body.user
   }
 
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,22 +1,45 @@
-// BlogList.jsx
+
 import React from 'react'
 import LikeButton from './LikeButton'
+import blogService from '../services/blogs'
 
 const BlogList = ({ blogs, setBlogs }) => {
-    const handleUpdate = (updatedBlog) => {
-        const newBlogs = blogs.map(blog => blog.id === updatedBlog.id ? updatedBlog : blog)
-        setBlogs(newBlogs)
+    const handleUpdate = async (blog) => {
+    const updatedBlog = {
+        author: blog.author,
+        likes: blog.likes + 1,
+        title: blog.title,
+        url: blog.url,
+        user: blog.user.id
     }
+        const response = await blogService.update(blog.id, updatedBlog)
+        const newBlogs = blogs.map(b => {
+            if (b.id === blog.id) {
+                return {...b, likes: b.likes + 1} 
+            }
+            return b
+        })
+        setBlogs(newBlogs.sort((a, b) => b.likes - a.likes))
+}
 
-    
-    const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes)
+
+    const handleRemove = async (id) => {
+        await blogService.remove(id)
+        const filteredBlogs = blogs.filter(blog => blog.id !== id)
+        filteredBlogs.sort((a, b) => b.likes - a.likes)
+        setBlogs(filteredBlogs)
+    }
 
     return (
         <div>
-            {sortedBlogs.map(blog => (
-                <div key={blog.id}>
-                    {blog.title} {blog.author}
+            {blogs.sort((a, b) => b.likes - a.likes).map(blog => (
+                <div key={blog.id} style={{ border: '2px solid #000000', padding: '10px', marginBottom: '5px' }}>
+                    <div>{blog.title} {blog.author}</div>
+                    <div>{blog.url}</div>
+                    <div>likes {blog.likes}</div>
+                    <div>{blog.user.name}</div>
                     <LikeButton blog={blog} onUpdate={handleUpdate} />
+                    <button onClick={() => handleRemove(blog.id)}>Remove</button>
                 </div>
             ))}
         </div>
@@ -24,4 +47,6 @@ const BlogList = ({ blogs, setBlogs }) => {
 }
 
 export default BlogList
+
+
 
