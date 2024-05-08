0f46a59bf5eb7fe9515da4c466a96115db55359d
Jake Anttonen <jajaant@utu.fi>
2024-05-08 04:24:29 +0300

    5.2 toimii

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -72,25 +72,26 @@ const App = () => {
 
   const handleLogOut = () => {
     window.localStorage.removeItem('loggedNoteappUser')
+    setUser(false)
   }
 
   return (
     <div>
-      {!user && <div>
-        <h1>log in to application</h1>
-        {loginForm()}
+      {user ? (
+        <div>
+          <h2>blogs</h2>
+          <p>{user.name} logged in <button onClick={handleLogOut}>logout</button></p>
+          {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog}/>)}
         </div>
-    }
-      {user && <div>
-        <h2>blogs</h2>
-        <p>{user.name} logged in <button onClick={() => handleLogOut()}>logout</button></p>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog}/>)}
-      </div>
-    }
-      
+      ) : (
+        <div>
+          <h1>log in to application</h1>
+          {loginForm()}
+        </div>
+      )}
     </div>
-  )
+  ) 
 }
 
 export default App
